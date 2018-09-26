21620675130d3c10a3fd48b6c691cef4576facd4
bwegrzanowski <bartosz.wegrzanowski@gmail.com>
2018-09-26 13:53:24 +0200

    initial project setup

---
+++ src/main/java/guru/springframework/sfgpetclinicproject/SfgPetclinicProjectApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetclinicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetclinicProjectApplication.class, args);
+	}
+}
