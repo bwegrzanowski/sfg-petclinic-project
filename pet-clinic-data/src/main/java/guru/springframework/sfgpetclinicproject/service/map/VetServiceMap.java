b0834f3a1ab3b6858c6319acce4a215f76661a26
bwegrzanowski <bartosz.wegrzanowski@gmail.com>
2018-10-08 15:15:12 +0200

    base map entity, first controllers and basic index pages

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicproject/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinicproject.service.map;
+
+import guru.springframework.sfgpetclinicproject.model.Vet;
+import guru.springframework.sfgpetclinicproject.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
